64f0c5991f6e2efbd2f7107c92c3103d685ab244
Vijayasarathi06 <141705162+Vijayasarathi06@users.noreply.github.com>
2023-08-08 12:44:12 +0530

    Arithmetic operators

---
+++ sarathi. datatype.py
@@ -0,0 +1,60 @@
+##Rectangle
+##l=int(input("Enter the value of length"))
+##b=int(input("Enter the value of breadth"))
+##area=l*b
+##print("area of rectangle",area)
+##perimeter=2*(l+b)
+##print("Perimeter of rectangle",perimeter)
+##Square
+##a=int(input("Enter the side of value"))
+##area=a**2
+##print("Area of the square",area)
+##perimeter=4*a
+##print("Perimeter of the square",perimeter)
+##Right angle triangle
+##b=int(input("Enter the value of base"))
+##h=int(input("Enter the value of height"))
+##l=int(input("Enter the value of hypotenuse"))
+##area=1/2*b*h
+##print("area of the right angle traingle",area)
+##perimeter=b+h+l
+##print("Perimeter of the right angle traingle",perimeter)
+##Parallelogram
+##b=int(input("Enter the value of base"))
+##h=int(input("Enter the value of height"))
+##area=b*h
+##print("area of the parallelogram",area)
+##Perimeter=2*(h+b)
+##print("Perimeter of the parallelogram",Perimeter)
+##Rhombus
+##a=int(input("Enter the side of value"))
+##d=int(input("Enter the value of diagnals"))
+##area=1/2*a*d
+##print("area of the rhombus",area)
+##perimeter=4*a
+##print("perimeter of the rhombus",perimeter)
+##Trapezium
+##a=int(input("Enter the value of parallel "))
+##b=int(input("Enter the value of sides"))
+##c=int(input("Enter the value of non parallel"))
+##d=int(input("Enter the value of sides"))
+##h=int(input("Enter the value of height"))
+##area=1/2*(a+b)*h
+##print("area of the trapezium",area)
+##perimeter=a+b+c+d
+##print("perimeter of the trapezium",perimeter)
+##Circle
+##r=int(input("Enter the value of radius"))
+##pie=int(input("Enter the value of pie"))
+##area=pie**2
+##print("area of the circle is",area)
+##perimeter=2*pie*r
+##print("perimeter of  the circle",perimeter)
+##Simple Interest
+##p=int(input("Enter the value of p"))
+##t=int(input("Enter the value of T"))
+##r=int(input("Enter the value of R"))
+##SI=p*t*r/100
+##print("value of simple interest",SI)
+
+
